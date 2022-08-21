d47a8ae7fa8543e2281b5a2de9e5ee415c25bee6
AngrieDucky <minakov.duck@gmail.com>
2022-08-21 13:17:10 +0200

    Add project files.

---
+++ TicTacChallenge/DrawBoard.cs
@@ -0,0 +1,214 @@
+﻿using System;
+
+namespace TicTacChallenge
+{
+    class DrawBoard
+    {
+        static string topLine = "   |   |   ";
+        static string bottomLine = "___|___|___";
+
+        static int[,] matrix =
+                     {
+                    {1,2,3 },
+                    {4,5,6 },
+                    {7,8,9 }
+                };
+
+        public DrawBoard()
+        {
+            Console.Clear();
+            this.resetMatrix();
+            this.drawBoard(matrix);
+        }
+
+        public void reDrawBoard()
+        {
+            Console.Clear();
+            this.drawBoard(matrix);
+        }
+
+
+        private void resetMatrix()
+        {
+            int counter = 1;
+            for (int i=0; i<matrix.GetLength(0); i++)
+            {
+                for (int j=0; j<matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = counter;
+                    counter++;
+                }
+            }
+        }
+
+        public int[] searchForNumber(int number)
+        {
+            int[] coordinates = new int[2];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if(matrix[i,j] == number && matrix[i,j] > 0)
+                    {
+                        coordinates[0] = i;
+                        coordinates[1] = j;
+                        return coordinates;
+                    }
+                }
+            }
+
+            return coordinates;
+        }
+
+        public bool checkIfNumber(int number)
+        {
+            bool isVacant = false;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == number && matrix[i, j] > 0)
+                    {
+                        isVacant = true;
+                        return isVacant;
+                    }
+                }
+            }
+            return isVacant;
+        }
+
+        public bool checkIfDraw()
+        {
+            bool draw = true;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j=0;j<matrix.GetLength(1); j++)
+                {
+                    if(matrix[i,j] > 0)
+                    {
+                        draw = false;
+                    }
+                }
+            }
+            return draw;
+        }
+
+        public void setNumber(int choice, bool player1)
+        {
+            int[] coordinates = searchForNumber(choice);
+            if (player1) {
+                matrix[coordinates[0], coordinates[1]] = -1; 
+            }
+            else
+            {
+                matrix[coordinates[0], coordinates[1]] = -2;
+            }
+        }
+
+        private void drawBoard(int[,] matrix)
+        {
+            int rowCounter = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.WriteLine(topLine);
+                int counter = 0;
+                string tempString = "";
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == -2)
+                    {
+                        tempString += " O ";
+                    }
+                    else if(matrix[i, j] == -1)
+                    {
+                        tempString += " X ";
+                    }
+                    else
+                    {
+                        tempString += " " + matrix[i, j] + " ";
+                    }
+
+                    if (counter < 2)
+                    {
+                        tempString += "|";
+                        counter++;
+                    }
+                }
+                Console.WriteLine(tempString);
+                rowCounter++;
+                if (rowCounter < 3)
+                {
+                    Console.WriteLine(bottomLine);
+                }
+                else
+                {
+                    Console.WriteLine(topLine);
+                }
+            }
+
+        }
+
+        public bool checkIfWon()
+        {
+            bool isWon = false;
+            if(checkRows(matrix) == true)
+            {
+                isWon = true;
+            }
+            if(checkColumns(matrix) == true)
+            {
+                isWon = true;
+            }
+            if(checkDiag(matrix) == true)
+            {
+                isWon = true;
+            }
+            return isWon;
+        }
+
+        private bool checkRows(int[,] matrix)
+        {
+            bool rowMatch = false;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int j = 1;
+                if ((matrix[i, j - 1] == matrix[i, j]) & (matrix[i, j] == matrix[i, j + 1]))
+                {
+                    rowMatch = true;
+                }
+            }
+            return rowMatch;
+        }
+
+        private bool checkColumns(int[,] matrix)
+        {
+            bool columnMatch = false;
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                int i = 1;
+                if ((matrix[i-1,j] == matrix[i,j]) & (matrix[i,j] == matrix[i + 1, j]))
+                {
+                    columnMatch = true;
+                }
+            }
+            return columnMatch;
+        }
+
+        private bool checkDiag(int[,] matrix)
+        {
+            bool diagMatch = false;
+            int i = 1;
+            if ((matrix[i-1,i-1] == matrix[i,i]) & (matrix[i,i] == matrix[i+1, i + 1]))
+            {
+                diagMatch = true;
+            }
+            if (matrix[i-1, i+1] == matrix[i,i] & (matrix[i,i] == matrix[i+1, i - 1]))
+            {
+                diagMatch = true;
+            }
+            return diagMatch;
+        }
+        
+    }
+}
---
+++ TicTacChallenge/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace TicTacChallenge
+{
+    class Program
+    {
+        
+        static string[,] x = new string[3, 3];
+        static string[,] o = new string[3, 3];
+
+        static char player1 = 'X';
+        static char player2 = 'O';
+
+        static bool noOneWon = true;
+        static bool draw = false;
+
+        static void Main(string[] args)
+        {
+            bool playerX = true;
+
+            while (true)
+            {
+                gameRound(playerX);
+
+                if (draw == false && playerX == false)
+                {
+                    Console.WriteLine("Congratulations, Player " + player1);
+                }
+                else if(draw == false && playerX == true)
+                {
+                    Console.WriteLine("Congratulations, Player " + player2);
+                }
+                else
+                {
+                    Console.WriteLine("It's a draw, no onw won");
+                }
+                Console.WriteLine("press any key to play again");
+                Console.ReadLine();
+            }
+
+        }
+
+        private static void gameRound(bool playerX)
+        {
+            // Start with drawing empty board
+            DrawBoard board = new DrawBoard();
+            noOneWon = true;
+            draw = false;
+
+            // ask players to make move
+            while (noOneWon == true && draw == false)
+            {
+                bool canBePlaced = false;
+                int playerCell;
+                while (canBePlaced != true)
+                {
+                    if (playerX)
+                    {
+                        playerCell = askForMove(player1);
+                    }
+                    else
+                    {
+                        playerCell = askForMove(player2);
+                    }
+
+                    if (board.checkIfNumber(playerCell) == true)
+                    {
+                        board.setNumber(playerCell, playerX);
+                        canBePlaced = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Can't place it in the occupied slot");
+                    }
+                }
+
+
+                board.reDrawBoard();
+                noOneWon = !board.checkIfWon();
+                draw = board.checkIfDraw();
+
+                playerX = !playerX;
+            }
+
+        }
+
+        private static int askForMove(char player)
+        {
+            int playerCell = -1;
+            while (playerCell < 0)
+            {
+                Console.WriteLine("Player " + player + " choose cell");
+            
+                int.TryParse(Console.ReadLine(), out playerCell);
+            }
+            return playerCell;
+        }
+        
+
+    }
+ }
